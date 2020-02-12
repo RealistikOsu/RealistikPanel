b3eb4b3e2f895d99dfd33f008851a0ceff65225f
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-02-12 20:07:06 +0100

    Redis connection added

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@
 from config import *
 import mysql.connector
 from colorama import init, Fore
+import redis
 
 init() #initialises colorama for colours
 
@@ -15,3 +16,18 @@ try:
 except Exception as e:
     print(f"{Fore.RED} Failed connecting to MySQL! Abandoning!\n Error: {e}{Fore.RESET}")
     exit()
+
+try:
+    r = redis.Redis(host=UserConfig["RedisHost"], port=UserConfig["RedisPort"], db=UserConfig["RedisDb"]) #establishes redis connection
+    print(f"{Fore.GREEN} Successfully connected to Redis!")
+except Exception as e:
+    print(f"{Fore.RED} Failed connecting to Redis! Abandoning!\n Error: {e}{Fore.RESET}")
+    exit()
+
+def DashData():
+    """Grabs all the values for the dashboard"""
+    response = {
+        "RegisteredUsers" : r.get("ripple:registered_users"),
+        "OnlineUsers" : r.get("ripple:online_users"),
+    }
+    return response
