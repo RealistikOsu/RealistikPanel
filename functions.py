2392bb6d513dc2da437a3211c30f66aa1146a544
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-05-04 13:46:15 +0100

    fix wipe

--- functions.py
+++ functions.py
@@ -934,9 +934,139 @@ def WipeAccount(AccId):
         mycursor.execute("DELETE FROM scores_ap WHERE userid = %s", (AccId,))
     #no stat reset until i fix it
     #now we reset stats... thats a bit of a query if i say so myself
-    mycursor.execute("UPDATE user_stats SET ranked_score_std = 0, playcount_std = 0, total_score_std = 0, replays_watched_std = 0, ranked_score_taiko = 0, playcount_taiko = 0, total_score_taiko = 0, replays_watched_taiko = 0, ranked_score_ctb = 0, playcount_ctb = 0, total_score_ctb = 0, replays_watched_ctb = 0, ranked_score_mania = 0, playcount_mania = 0, total_score_mania = 0, replays_watched_mania = 0, total_hits_std = 0, total_hits_taiko = 0, total_hits_ctb = 0, total_hits_mania = 0, unrestricted_pp = 0, level_std = 0, level_taiko = 0, level_ctb = 0, level_mania = 0, playtime_std = 0. playtime_taiko = 0, playtime_ctb = 0, playtime_mania = 0, avg_accuracy_std = 0.000000000000, avg_accuracy_taiko = 0.000000000000, avg_accuracy_ctb = 0.000000000000, avg_accuracy_mania = 0.000000000000, pp_std = 0, pp_taiko = 0, pp_ctb = 0, pp_mania = 0 WHERE id = %s", (AccId,))
+    mycursor.execute("""UPDATE
+            user_stats
+        SET
+            ranked_score_std = 0,
+            playcount_std = 0,
+            total_score_std = 0,
+            replays_watched_std = 0,
+            ranked_score_taiko = 0,
+            playcount_taiko = 0,
+            total_score_taiko = 0,
+            replays_watched_taiko = 0,
+            ranked_score_ctb = 0,
+            playcount_ctb = 0,
+            total_score_ctb = 0,
+            replays_watched_ctb = 0,
+            ranked_score_mania = 0,
+            playcount_mania = 0,
+            total_score_mania = 0,
+            replays_watched_mania = 0,
+            total_hits_std = 0,
+            total_hits_taiko = 0,
+            total_hits_ctb = 0,
+            total_hits_mania = 0,
+            unrestricted_pp = 0,
+            level_std = 0,
+            level_taiko = 0,
+            level_ctb = 0,
+            level_mania = 0,
+            playtime_std = 0,
+            playtime_taiko = 0,
+            playtime_ctb = 0,
+            playtime_mania = 0,
+            avg_accuracy_std = 0.000000000000,
+            avg_accuracy_taiko = 0.000000000000,
+            avg_accuracy_ctb = 0.000000000000,
+            avg_accuracy_mania = 0.000000000000,
+            pp_std = 0,
+            pp_taiko = 0,
+            pp_ctb = 0,
+            pp_mania = 0
+        WHERE
+            id = %s
+    """, (AccId,))
+
     if UserConfig["HasRelax"]:
-        mycursor.execute("UPDATE user_stats SET ranked_score_std = 0, playcount_std = 0, total_score_std = 0, replays_watched_std = 0, ranked_score_taiko = 0, playcount_taiko = 0, total_score_taiko = 0, replays_watched_taiko = 0, ranked_score_ctb = 0, playcount_ctb = 0, total_score_ctb = 0, replays_watched_ctb = 0, ranked_score_mania = 0, playcount_mania = 0, total_score_mania = 0, replays_watched_mania = 0, total_hits_std = 0, total_hits_taiko = 0, total_hits_ctb = 0, total_hits_mania = 0, unrestricted_pp = 0, level_std = 0, level_taiko = 0, level_ctb = 0, level_mania = 0, playtime_std = 0. playtime_taiko = 0, playtime_ctb = 0, playtime_mania = 0, avg_accuracy_std = 0.000000000000, avg_accuracy_taiko = 0.000000000000, avg_accuracy_ctb = 0.000000000000, avg_accuracy_mania = 0.000000000000, pp_std = 0, pp_taiko = 0, pp_ctb = 0, pp_mania = 0 WHERE id = %s", (AccId,))
+        mycursor.execute("""UPDATE
+                rx_stats
+            SET
+                ranked_score_std = 0,
+                playcount_std = 0,
+                total_score_std = 0,
+                replays_watched_std = 0,
+                ranked_score_taiko = 0,
+                playcount_taiko = 0,
+                total_score_taiko = 0,
+                replays_watched_taiko = 0,
+                ranked_score_ctb = 0,
+                playcount_ctb = 0,
+                total_score_ctb = 0,
+                replays_watched_ctb = 0,
+                ranked_score_mania = 0,
+                playcount_mania = 0,
+                total_score_mania = 0,
+                replays_watched_mania = 0,
+                total_hits_std = 0,
+                total_hits_taiko = 0,
+                total_hits_ctb = 0,
+                total_hits_mania = 0,
+                unrestricted_pp = 0,
+                level_std = 0,
+                level_taiko = 0,
+                level_ctb = 0,
+                level_mania = 0,
+                playtime_std = 0,
+                playtime_taiko = 0,
+                playtime_ctb = 0,
+                playtime_mania = 0,
+                avg_accuracy_std = 0.000000000000,
+                avg_accuracy_taiko = 0.000000000000,
+                avg_accuracy_ctb = 0.000000000000,
+                avg_accuracy_mania = 0.000000000000,
+                pp_std = 0,
+                pp_taiko = 0,
+                pp_ctb = 0,
+                pp_mania = 0
+            WHERE
+                id = %s
+        """, (AccId,))
+    if UserConfig["HasAutopilot"]:
+        mycursor.execute("""UPDATE
+                ap_stats
+            SET
+                ranked_score_std = 0,
+                playcount_std = 0,
+                total_score_std = 0,
+                replays_watched_std = 0,
+                ranked_score_taiko = 0,
+                playcount_taiko = 0,
+                total_score_taiko = 0,
+                replays_watched_taiko = 0,
+                ranked_score_ctb = 0,
+                playcount_ctb = 0,
+                total_score_ctb = 0,
+                replays_watched_ctb = 0,
+                ranked_score_mania = 0,
+                playcount_mania = 0,
+                total_score_mania = 0,
+                replays_watched_mania = 0,
+                total_hits_std = 0,
+                total_hits_taiko = 0,
+                total_hits_ctb = 0,
+                total_hits_mania = 0,
+                unrestricted_pp = 0,
+                level_std = 0,
+                level_taiko = 0,
+                level_ctb = 0,
+                level_mania = 0,
+                playtime_std = 0,
+                playtime_taiko = 0,
+                playtime_ctb = 0,
+                playtime_mania = 0,
+                avg_accuracy_std = 0.000000000000,
+                avg_accuracy_taiko = 0.000000000000,
+                avg_accuracy_ctb = 0.000000000000,
+                avg_accuracy_mania = 0.000000000000,
+                pp_std = 0,
+                pp_taiko = 0,
+                pp_ctb = 0,
+                pp_mania = 0
+            WHERE
+                id = %s
+        """, (AccId,))
+    
     mydb.commit()
 
 def ResUnTrict(id : int):
