41fbea4ab66673cdeb06d128beb1c5ff8490b724
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-06-24 22:34:58 +0100

    1 char typo

--- functions.py
+++ functions.py
@@ -1229,7 +1229,7 @@ def DeleteAccount(id : int):
     mycursor.execute("DELETE FROM users_relationships WHERE user1 = %s OR user2 = %s", (id, id,))
     mycursor.execute("DELETE FROM user_badges WHERE user = %s", (id,))
     mycursor.execute("DELETE FROM user_clans WHERE user = %s", (id,))
-    mycursor.execute("DELETE FROM user_stats WHERE id = %s", (id,))
+    mycursor.execute("DELETE FROM users_stats WHERE id = %s", (id,))
     if UserConfig["HasRelax"]:
         mycursor.execute("DELETE FROM scores_relax WHERE userid = %s", (id,))
         mycursor.execute("DELETE FROM rx_stats WHERE id = %s", (id,))
