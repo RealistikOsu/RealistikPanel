eb7f73efcd22772f7b8a293c0c6df8f345e60dad
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-07-05 20:37:48 +0100

    closes #52

--- functions.py
+++ functions.py
@@ -1536,7 +1536,7 @@ def GetUserBadges(AccountID: int):
         Badges.append(badge[0])
 
     #so we dont run into errors where people have no/less than 6 badges
-    while len(Badges) != 6:
+    while len(Badges) < 6:
         Badges.append(0)
     return Badges
 
