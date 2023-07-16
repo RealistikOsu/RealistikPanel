2e0465c490ffa26e94a197927aac22b70eab6e1d
lenforiee <lenforiee@gmail.com>
2023-07-17 00:01:55 +0200

    remove comment

--- panel/common/__init__.py
+++ panel/common/__init__.py
@@ -4,6 +4,3 @@ from . import cryprography
 from . import threads
 from . import time
 from . import utils
-
-# from . import responses THIS DOESNT WORK AND I DONT KNOW WHY
-# IT DOESNT EVEN MATTER HOW HARD I TRY
